57e4487ecc0d08755e0aab68fc8b0fa7843adfa0
Karan Melo <karan_melo@hotmail.com>
2021-10-10 14:54:21 -0300

    feat: add style guide

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = Object.freeze({
+  white: '#FFFFF1',
+  blue: '#4480E3',
+  purple: '#974CFD',
+  dark: '#333333',
+  gray: '#D9D9D9',
+  red: '#F44336',
+})
---
+++ src/styles/styleGuide.ts
@@ -0,0 +1,30 @@
+import { colors } from './colors'
+
+export const sg = Object.freeze({
+  colors,
+  fontFamily: {
+    primary: 'Roboto',
+  },
+  fontSize: {
+    default: '16px',
+    smaller: '12px',
+    small: '14px',
+    medium: '18px',
+    large: '20px',
+    xlarge: '24px',
+    xxlarge: '28px',
+  },
+  spacing: {
+    default: '8px',
+    xsmall: '2px',
+    small: '4px',
+    medium: '8px',
+    large: '16px',
+    xlarge: '24px',
+    xxlarge: '32px',
+    xxxlarge: '40px',
+  },
+  shadows: {
+    main: '0px -1px 14px 0px #00000014',
+  },
+})
