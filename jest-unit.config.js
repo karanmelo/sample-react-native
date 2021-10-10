aecb3d1a7193ae33339453b8a8c3959758850d25
Karan Melo <karan_melo@hotmail.com>
2021-10-10 15:51:04 -0300

    fix: jest configuration

---
+++ jest-unit.config.js
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const config = require('./jest.config.js')
+config.testMatch = ['**/*.spec.ts']
+module.exports = config
---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/__tests__', '<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  // transform: {
+  //   '.+\\.ts$': 'ts-jest',
+  // },
+  moduleNameMapper: {
+    '@/__tests__/(.*)': '<rootDir>/__tests__/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+    'src/(.*)': '<rootDir>/src/$1',
+    '__tests__/(.*)': '<rootDir>/__tests__/$1',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+}
