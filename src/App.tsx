a48a75500bef4d5c37d4d365879912fc00ea028a
Karan Melo <karan_melo@hotmail.com>
2021-10-11 09:26:26 -0300

    feat: route-based deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
 import React from 'react'
 
-import {
-  SafeAreaView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { Router } from './router'
+import { ThemeProvider } from './store/'
 
-const App: React.FC = () => {
-  const isDarkMode = useColorScheme() === 'dark'
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  }
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View
-        style={{
-          backgroundColor: isDarkMode ? Colors.black : Colors.white,
-        }}
-      >
-        <Text>Sample React Native App</Text>
-      </View>
-    </SafeAreaView>
-  )
-}
+const App: React.FC = () => (
+  <ThemeProvider>
+    <Router />
+  </ThemeProvider>
+)
 
 export default App
