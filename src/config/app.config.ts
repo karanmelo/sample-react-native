2f9ded8c0a11e8da0207b1057ade8e2532961924
Karan Melo <karan_melo@hotmail.com>
2021-10-12 22:40:52 -0300

    fix: add RN_APP_BASE_URL on config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,9 +3,11 @@ import Config from 'react-native-config'
 type AppConfigType = {
   RN_APP_ENV: 'production' | 'development'
   RN_APP_USE_MOCKS: boolean
+  RN_APP_BASE_URL: string
 }
 
 export const appConfig = {
   RN_APP_ENV: Config.RN_APP_ENV || 'development',
   RN_APP_USE_MOCKS: Config.RN_APP_USE_MOCKS === 'true',
+  RN_APP_BASE_URL: Config.RN_APP_BASE_URL || 'http://localhost:8081/',
 } as AppConfigType
