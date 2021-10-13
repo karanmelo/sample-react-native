a903e40a592a7269a7ae6e2863c97699c2bc40c6
Karan Melo <karan_melo@hotmail.com>
2021-10-12 22:47:18 -0300

    fix: background color on avoiding view component

--- src/components/keyboardAvoidingView/styles.ts
+++ src/components/keyboardAvoidingView/styles.ts
@@ -2,11 +2,9 @@ import { KeyboardAvoidingView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components'
 
-import { sg } from '../../styles/styleGuide'
-
 export const SafeAreaViewWrapper = styled(SafeAreaView)`
   flex: 1;
-  background-color: ${sg.colors.dark};
+  background: ${props => props.theme.colors.primary};
 `
 export const KeyboardAvoidingViewWrapper = styled(KeyboardAvoidingView)`
   flex: 1;
