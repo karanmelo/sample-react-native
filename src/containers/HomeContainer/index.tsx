bf5aa1e73510fbad7852d5ae808fa81c5b40c6cc
Karan Melo <karan_melo@hotmail.com>
2021-10-15 20:06:10 -0300

    feat: create home container

---
+++ src/containers/HomeContainer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Text } from '../../components'
+import { BaseLayout } from '../../components/layouts'
+import * as S from './styles'
+
+export const HomeContainer: React.FC = () => (
+  <BaseLayout>
+    <Text>Home</Text>
+  </BaseLayout>
+)
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,2 +1,3 @@
 export { SigInContainer } from './SigInContainer'
+export { HomeContainer } from './HomeContainer'
 export { SettingsContainer } from './SettingsContainer'
