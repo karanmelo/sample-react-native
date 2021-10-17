ce2fd015ff28c3c13f1af2094dcdfe4f5fa8090a
Karan Melo <karan_melo@hotmail.com>
2021-10-16 21:02:10 -0300

    feat: create global line style

--- src/styles/utils.ts
+++ src/styles/utils.ts
@@ -8,3 +8,9 @@ export type RowProps = {
 export const Row = styled(View)<RowProps>`
   margin-top: ${props => (props.marginTop ? `${props.marginTop}px` : 0)};
 `
+
+export const Line = styled(View)`
+  width: 100%;
+  height: 1px;
+  background-color: ${({ theme }) => theme.colors.button};
+`
